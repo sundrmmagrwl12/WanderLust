f246660a184fee08a5c02d4fa54a6c91707c7962
Sundram Singhal <sundramsinghal944@gmail.com>
2026-07-09 20:28:46 +0530

    Configure dynamic port for deployment

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ const sessionOption = {
   store: store,
   secret: process.env.SECRET,
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   cookie: {
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -141,6 +141,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("./error.ejs", { err });
 });
 
-app.listen(8080, () => {
-  // console.log("Server is working on port : 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
